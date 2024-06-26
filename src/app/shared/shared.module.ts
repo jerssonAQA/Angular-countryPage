822b2c83ffe6b19bab97450b42eeb870ef1814b9
enyoiner <jersson.9.5@gmail.com>
2024-06-25 23:41:14 -0500

    Router Module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,20 +1,25 @@
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/aboutPage/aboutPage.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
-    AboutPageComponent
+    AboutPageComponent,
+    SidebarComponent,
   ],
   imports: [
-    RouterModule
+    RouterModule,
+    CommonModule
   ],
   exports:[
     AboutPageComponent,
-    HomePageComponent
+    HomePageComponent,
+    SidebarComponent
   ]
 })
 export class SharedModule { }
